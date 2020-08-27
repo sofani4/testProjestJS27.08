701a172a54dc0ca14ebb4bc3395d74b539f305ca
Sofi <sofani4@gmail.com>
2020-08-27 16:19:24 +0500

    первый коммит

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let number = 5;
+const leftBorderWidth = 1;
+var age = 25;
+console.log(`I am ${age} years old`); // I am 25 years old
+
+var a = 5;
+var b = 10;
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
+// "Fifteen is 15 and not 20.
